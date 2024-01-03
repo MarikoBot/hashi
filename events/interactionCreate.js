361dd28c695885822981128e390f8e2e372ee74f
elouannh <elouannhosta01@gmail.com>
2024-01-03 18:41:27 +0100

    Updated the .gitignore and the .npmignore

--- events/interactionCreate.js
+++
@@ -1,4 +0,0 @@
-const {HashiEvent} = require("../lib/index");
-
-module.exports = new HashiEvent('interactionCreate')
-    .setCallbackFunction((client, interaction) => client.detectAndLaunchCommand(interaction));
